92746fb0598762da04bc2a1b61d882331e673ddc
Joao Emilio <joaoemilio@gmail.com>
2021-09-28 18:01:44 -0300

    Enrich CS

--- EnrichSymbolData.py
+++ EnrichSymbolData.py
@@ -175,9 +175,9 @@ def enrich(symbol, cs, data, doc_cs, dataws):
             }
             for i in [5,10,15,20,25,30,50,100,150,200]:
                 if doc_aug["future"][p]['high']['d'] >= i/1000:
-                    doc_aug["future"][p]['buy'] = 1
+                    doc_aug["future"][p]['buy{i}'] = 1
                 else:
-                    doc_aug["future"][p]['buy'] = 0
+                    doc_aug["future"][p][f'buy{i}'] = 0
 
     doc_aug["version"] = "1.0.0"
 
